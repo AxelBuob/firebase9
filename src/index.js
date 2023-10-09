caec8f03a76436b948262edf993d340cd742713f
Axel Buob <axelbuob@axels-macbook-pro.home>
2023-10-09 20:53:37 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCbWA9PLeN-6LS0gEfvvS3mvnUWDIdSbNA",
+    authDomain: "fir-9-dojo-de86c.firebaseapp.com",
+    projectId: "fir-9-dojo-de86c",
+    storageBucket: "fir-9-dojo-de86c.appspot.com",
+    messagingSenderId: "37345275342",
+    appId: "1:37345275342:web:2739c3c05f33cb88400f9f"
+  };
+
+  initializeApp(firebaseConfig);
