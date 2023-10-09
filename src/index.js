2191505000e35cb741358033c28cfda68faedcff
Axel Buob <axelbuob@axels-macbook-pro.home>
2023-10-09 22:03:32 +0200

    lesson 5

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, getDocs } from 'firebase/firestore';
-
+import { initializeApp } from 'firebase/app'
+import {
+  getFirestore, collection, getDocs,
+  addDoc, deleteDoc, doc
+} from 'firebase/firestore'
 
 const firebaseConfig = {
     apiKey: "AIzaSyCbWA9PLeN-6LS0gEfvvS3mvnUWDIdSbNA",
@@ -10,25 +12,53 @@ const firebaseConfig = {
     messagingSenderId: "37345275342",
     appId: "1:37345275342:web:2739c3c05f33cb88400f9f"
   };
+// init firebase
+initializeApp(firebaseConfig)
 
-// init firebase app
-initializeApp(firebaseConfig);
-
-// init services 
-const db = getFirestore();
+// init services
+const db = getFirestore()
 
 // collection ref
-const colRef = collection(db, 'Books');
+const colRef = collection(db, 'Books')
 
-// get collection data 
+// get collection data
 getDocs(colRef)
-    .then((snapshot) => {
-        let books = [];
-        snapshot.docs.forEach((doc) => {
-            books.push({ ...doc.data(), id: doc.id });
-        });
-        console.log(books);
+  .then(snapshot => {
+    // console.log(snapshot.docs)
+    let books = []
+    snapshot.docs.forEach(doc => {
+      books.push({ ...doc.data(), id: doc.id })
     })
-    .catch(err => {
-        console.error(err.message);
-    });
+    console.log(books)
+  })
+  .catch(err => {
+    console.log(err.message)
+  })
+
+// adding docs
+const addBookForm = document.querySelector('.add')
+addBookForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  addDoc(colRef, {
+    title: addBookForm.title.value,
+    author: addBookForm.author.value,
+  })
+  .then(() => {
+    addBookForm.reset()
+  })
+})
+
+// deleting docs
+const deleteBookForm = document.querySelector('.delete')
+deleteBookForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const docRef = doc(db, 'Books', deleteBookForm.id.value)
+    console.log(docRef);
+
+  deleteDoc(docRef)
+    .then(() => {
+      deleteBookForm.reset()
+    })
+})
